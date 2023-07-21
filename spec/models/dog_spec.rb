922d3f25062507fd7c0413086aafc15cf80e0617
miguelosvaldo <miguel.o.molina@icloud.com>
2023-07-21 14:12:03 -0700

    updates api validations

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -2,23 +2,62 @@ require 'rails_helper'
 
 RSpec.describe Dog, type: :model do
   it "should validate name" do
-    dog = Dog.create
-    expect(dog.errors[:name]).to_not be_empty
+     user1 = User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password")
+      dog = Dog.create(
+        description:'we can fill it later rocky',
+        age:7,
+        vaccination_status:true,
+        image: 'some url goes here',
+        user_id: user1.id
+      )
+      expect(dog.errors[:name]).to_not be_empty
   end
+  
   it "should validate age" do
-    dog = Dog.create
+    user1 = User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password")
+    dog = Dog.create(
+        name:'Rocky',
+        description:'we can fill it later rocky',
+        vaccination_status:true,
+        image: 'some url goes here',
+        user_id: user1.id
+    )    
     expect(dog.errors[:age]).to_not be_empty
   end
+  
   it "should validate description" do
-    dog = Dog.create
+    user1 = User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password")
+    dog = Dog.create(
+        name:'Rocky',
+        age:7,
+        vaccination_status:true,
+        image: 'some url goes here',
+        user_id: user1.id
+    )   
     expect(dog.errors[:description]).to_not be_empty
   end
+  
   it "should validate image" do
-    dog = Dog.create
+    user1 = User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password")
+    dog = Dog.create(
+        name:'Rocky',
+        description:'we can fill it later rocky',
+        age:7,
+        vaccination_status:true,
+        user_id: user1.id
+    )    
     expect(dog.errors[:image]).to_not be_empty
   end
+  
   it "should validate vaccination status" do
-    dog = Dog.create
+    user1 = User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password")
+    dog = Dog.create(
+        name:'Rocky',
+        description:'we can fill it later rocky',
+        age:7,
+        image: 'some url goes here',
+        user_id: user1.id
+    )    
     expect(dog.errors[:vaccination_status]).to_not be_empty
   end
 
