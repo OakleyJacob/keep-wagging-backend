b91b465ce64f42091a8c207f7356311062932235
miguelosvaldo <miguel.o.molina@icloud.com>
2023-07-27 10:21:45 -0700

    here you go jacob

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "https://dogpoundheavenfrontend.onrender.com/"
+      origins '*'
       resource '*',
       headers: ["Authorization"],
       expose: ["Authorization"],
